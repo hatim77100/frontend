2c99ffa597754452942a9115f08b02b48fae911c
hatim77100 <najoua-hatim@hotmail.fr>
2022-08-27 23:50:30 +0200

    Site KYOTO

---
+++ 8-siteWebKyoto/main.js
@@ -0,0 +1,12 @@
+const btnMenu = document.querySelector(".logo-menu");
+const menu = document.querySelector(".liste-nav");
+const allLinks = document.querySelectorAll(".item-nav");
+btnMenu.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
+
+allLinks.forEach((item) => {
+  item.addEventListener("click", () => {
+    menu.classList.toggle("active");
+  });
+});
